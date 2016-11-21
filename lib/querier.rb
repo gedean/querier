2d185c099b31a060ff0ab9d717a31d800d1e6d4e
Gedean Dias <gedean.dias@gmail.com>
2016-11-21 08:57:25 -0300

    Enhanced query class and added at least a test

--- lib/querier.rb
+++ lib/querier.rb
@@ -1,6 +1,8 @@
 class Querier
+  
   PARAM_NAME_INDEX = 0
   PARAM_VALUE_INDEX = 1
+
   attr_reader :query_template, :query_params
   
   def initialize **template_query_params
@@ -8,7 +10,7 @@ class Querier
   end
 
   def execute
-    ActiveRecord::Base.connection.select_all build_query(query_template: @query_template, query_params: @query_params)
+    ActiveRecord::Base.connection.select_all fill_query_params(query_template: @query_template, query_params: @query_params)
   end   
 
   def structured_results
@@ -18,22 +20,23 @@ class Querier
     structured_results
   end  
 
-  def to_sanitized_sql
-    to_sql.gsub(/\n/, ' ').gsub(/\t/, ' ')
-  end
-
   def to_sql
-    build_query(query_template: @query_template, query_params: @query_params)
+    fill_query_params(query_template: @query_template, query_params: @query_params)
   end
 
   private
   
-  def build_query query_template:, query_params:
+  def get_param_value raw_query_param
+    # where's String#quote when we need it?
+    raw_query_param.class.eql?(String) ? "'#{raw_query_param.to_s}'" : raw_query_param.to_s
+  end
+
+  def fill_query_params query_template:, query_params:
     query = query_template.dup
     
     query_params.each do |query_param|
       query_param_name = query_param[PARAM_NAME_INDEX].to_s
-      query_param_value = query_param[PARAM_VALUE_INDEX].class.eql?(String) ? "'#{query_param[PARAM_VALUE_INDEX].to_s}'" : query_param[PARAM_VALUE_INDEX].to_s
+      query_param_value = get_param_value(query_param[PARAM_VALUE_INDEX])
 
       query.gsub! /{\?#{query_param_name}}/, query_param_value
     end
--- spec/querier_spec.rb
+++ spec/querier_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../lib/querier'
 
+TEST_USER_NAME = 'foo'
+VALID_QUERY = "SELECT * FROM users	WHERE name = '#{TEST_USER_NAME}' AND active = true"
+
+class UserQuerier < Querier
+  def initialize user_name:, active:
+    @query_template = "SELECT * FROM users	WHERE name = {?user_name} AND active = {?active}"
+    super
+  end
+end
+
 describe Querier do
-	
+
+before :each do
+	@user_querier = UserQuerier.new(user_name: TEST_USER_NAME, active: true)
+end
+
+	it 'Returns a valid query' do
+		expect(@user_querier.to_sql).to eq VALID_QUERY
+	end
 end
