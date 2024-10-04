dbd5630c750857c9876888958df3eb437cd07855
Gedean Dias <gedean.dias@gmail.com>
2024-10-03 21:37:21 -0300

    Refactor Querier Class to improve code readability and maintainability

--- lib/querier.rb
+++ lib/querier.rb
@@ -1,9 +1,6 @@
 require 'active_record'
 
 class Querier
-  PARAM_NAME_INDEX = 0
-  PARAM_VALUE_INDEX = 1
-
   @active_record_class = ActiveRecord::Base
   # based on rubocop's tips at: https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/ClassVars
   # solution here: https://www.ruby-lang.org/en/documentation/faq/8/
@@ -19,75 +16,33 @@ class Querier
     @query = fill_query_params
   end
 
-  def execute
-    @active_record_class.connection.execute(@query)
-  end
-
-  def exec_query
-    decorate_dataset_format(@active_record_class.connection.exec_query(@query))
-  end
-
-  def select_all
-    decorate_dataset_format(@active_record_class.connection.select_all(@query))
-  end
-
-  def select_one
-    @active_record_class.connection.select_one(@query)
-  end
-
-  def select_sole
-    @active_record_class.connection.select_sole(@query)
-  end
-
-  def select_values
-    @active_record_class.connection.select_values(@query)
-  end
-
-  def select_rows
-    @active_record_class.connection.select_rows(@query)
-  end
-
-  def select_value
-    @active_record_class.connection.select_value(@query)
-  end
+  def execute = @active_record_class.connection.execute(@query)
+  def exec_query = decorate_dataset_format(@active_record_class.connection.exec_query(@query))
+  def select_all = decorate_dataset_format(@active_record_class.connection.select_all(@query))
+  def select_one = @active_record_class.connection.select_one(@query)
+  def select_values = @active_record_class.connection.select_values(@query)
+  def select_rows = @active_record_class.connection.select_rows(@query)
+  def select_value = @active_record_class.connection.select_value(@query)
 
   private
 
   def decorate_dataset_format(dataset)
-    def dataset.as_hash
-      map(&:symbolize_keys)
-    end
-
-    def dataset.as_struct
-      map { |record| OpenStruct.new(record) }
-    end
-
+    def dataset.as_hash = map(&:symbolize_keys)
+    def dataset.as_struct = map { |record| OpenStruct.new(record) }
     dataset
-  end
-
-  def get_param_value(raw_query_param:, quotefy_param: true)
-    if raw_query_param.instance_of?(String) && quotefy_param
-      @active_record_class.connection.quote(raw_query_param.to_s)
-    else
-      raw_query_param.to_s
-    end
   end
 
   def fill_query_params
     query = @query_template.dup
 
-    @query_params.each_pair do |query_param|
-      query_param_name = query_param[PARAM_NAME_INDEX].to_s
-
-      query.gsub!(/\${#{query_param_name}}/,
-                  get_param_value(raw_query_param: query_param[PARAM_VALUE_INDEX], 
-                                  quotefy_param: true))
-
-      query.gsub!(/\${#{query_param_name}\/no_quote}/,
-                  get_param_value(raw_query_param: query_param[PARAM_VALUE_INDEX],
-                                  quotefy_param: false))
+    @query_params.each do |param_name, param_value|
+      placeholder = "${#{param_name}}"
+      placeholder_no_quote = "${#{param_name}/no_quote}"
+  
+      query.gsub!(placeholder, @active_record_class.connection.quote(param_value.to_s))
+      query.gsub!(placeholder_no_quote, param_value.to_s)
     end
-
+  
     query
   end
 end
--- querier.gemspec
+++ querier.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name          = 'querier'
-  s.version       = '0.4.3'
-  s.date          = '2022-03-17'
+  s.version       = '0.4.4'
+  s.date          = '2024-10-03'
   s.summary       = 'Active Record Querier'
   s.description   = 'Active Record queries with variable number of params'
   s.authors       = ['Gedean Dias']
